6ddc9c04bdab76bb1f3effff837c81b3fd6489a7
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2021-07-19 15:34:03 -0400

    made progress on the server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,25 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// import Apollo Server
+const { ApolloServer } = require('apollo-server-express');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+// integrate our Apollo Server with the Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
