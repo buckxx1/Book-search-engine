fed08a9fce2d3f2f03c1a36573607d342c8d0e7b
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2021-07-19 21:27:12 -0400

    the resolvers index and typedefs are looking correct

---
+++ server/routes/schemas/typeDefs.js
@@ -0,0 +1,51 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create typeDefs
+const typeDefs = gql`
+    input BookInput {
+        bookId: String!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+    
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: BookInput): User
+        deleteBook(bookId: String!): User
+    }
+`;
+
+// export typeDefs
+module.exports = typeDefs;
